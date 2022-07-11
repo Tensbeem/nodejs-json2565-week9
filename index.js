000f12e14b9d6fd802f6ee82bdfbdf2f22792a5f
Tensbeam <tensrrt123@gmail.com>
2022-07-11 17:36:49 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const http = require('http')
 const msg = require('./message.js')
-const {loadUser}  = require('./user_models')
+//const {loadUser}  = require('./user_models')
+const {loadVnl2022} = require('./vnl2022_model')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-    const JsonContent = JSON.stringify(loadUser())
-
+    //const JsonContent = JSON.stringify(loadUser())
+    const JsonContent = JSON.stringify(loadVnl2022())
     res.writeHead(200, { 'Content-Type':'text/plain' })
-    res.write(`${JsonContent }`)
+    //res.write(`${JsonContent }`)
+    res.write(`${JsonContent}`)
     res.end()
 })
 
